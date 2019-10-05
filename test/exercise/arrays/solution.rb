500c61355270c8c4445a99071a13fc057a476686
formindev <formindeve@gmail.com>
2019-10-05 15:40:04 +0400

    Added Replace function

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,9 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = 0;
+        array.each { |item| if ( max < item ) then max = item end }
+             .map { |item| item > 0 ? max : item }
       end
 
       def search(_array, _query)
