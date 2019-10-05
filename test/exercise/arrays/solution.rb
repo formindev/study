87e801ee2089eddbc0a86637875bf0fb13566adb
formindev <formindeve@gmail.com>
2019-10-05 19:16:52 +0400

    Added bsearch function

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,13 +2,28 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = 0;
-        array.each { |item| if ( max < item ) then max = item end }
+        max = 0
+        array.each { |item| max = item if max < item }
              .map { |item| item > 0 ? max : item }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.empty?
+
+        first = 0
+        last = array.length - 1
+
+        while first <= last
+
+          middle = first + (last - first) / 2
+          return middle if array[middle] == query
+
+          if array[middle] < query
+            first = middle + 1
+          else last = middle - 1
+          end
+        end
+        -1
       end
     end
   end
