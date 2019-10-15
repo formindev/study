8d9bb1a4c060d2c3ffe6b6316dcf0cce74832696
formindev <formindeve@gmail.com>
2019-10-15 20:42:41 +0400

    Added fp2 solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,15 +4,12 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(array)
-        arr = array.select { |film| film['country'].to_s.split(',').length > 1 && film['rating_kinopoisk'].to_f > 0 }
-                   .map { |film| film['rating_kinopoisk'].to_f }
-        arr.reduce(:+) / arr.length
+      def rating(_array)
+        0
       end
 
-      def chars_count(films, threshold)
-        films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
-             .map { |film| film['name'].count('и') }.reduce(:+)
+      def chars_count(_films, _threshold)
+        0
       end
     end
   end
--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,30 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        for i in 0..length - 1 do
+          yield self[i]
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        return_array = MyArray.new
+        my_each { |item| return_array << yield(item) }
+        return_array
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        return_array = MyArray.new
+        my_map { |item| return_array << item unless item.nil? }
+        return_array
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        my_each { |item| acc.nil? ? acc = self[0] : acc = yield(acc, item) }
+        acc
       end
     end
   end
