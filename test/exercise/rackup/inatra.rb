051fd91d6169ff2d4cd81d6543c6c4d80e501639
formindev <formindeve@gmail.com>
2019-10-15 22:28:02 +0400

    Added rackup functions

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,46 @@
 module Inatra
   class << self
-    def routes
+    @@routes = {}
+
+    def routes(&block)
+      instance_eval(&block)
+    end
+
+    def add_route(verb, path, &handler)
+      @@routes[verb] ||= {}
+      @@routes[verb][path] = handler
+    end
+
+    def get(path, &handler)
+      add_route("GET", path, &handler)
+    end
+
+    def post(path, &handler)
+      add_route("POST", path, &handler)
+    end
+
+    def put(path, &handler)
+      add_route("PUT", path, &handler)
+    end
+
+    def patch(path, &handler)
+      add_route("PATCH", path, &handler)
+    end
+
+    def delete(path, &handler)
+      add_route("DELETE", path, &handler)
+    end
+
+    def head(path, &handler)
+      add_route("HEAD", path, &handler)
     end
 
     def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+      handler = @@routes[verb][requested_path]
+      handler.call
     end
   end
 end
